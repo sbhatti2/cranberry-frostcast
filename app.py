c4893c4bb0ae95a4b47f64b00f1a01345e399d05
sbhatti2 <sandeepbhatti2408@gmail.com>
2026-03-12 14:45:23 -0400

    Fix: Using pytz time variables, target 10z UTC morning, using 1 hour past run time, and implement critical window fallback for HRRR curve

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ Created on Wed Jan  7 15:38:55 2026
 # %% import modules
 import pandas as pd
 import numpy as np
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta,time
 import matplotlib
 import matplotlib.pyplot as plt
 import joblib
@@ -34,12 +34,30 @@ def run_forecast(BOG_LAT, BOG_LON,current_run_time = None):
     model = package['model']
     scaler = package['scaler']
     # FETCH LIVE WEATHER DATA (HRRR Model)
-    now_utc = datetime.utcnow()
-    target_utc = now_utc.replace(hour=10, minute=0, second=0, microsecond=0)
-    target_date_str = (target_utc - timedelta(hours=6)).strftime('%A, %b %d') # CST Date
-    if now_utc.hour >= 9: #the if logic is to ensure you are not forecasting time that has already passed
-        target_utc += timedelta(days=1)
-    run_time = (now_utc - timedelta(hours=2)).replace(minute=0, second=0, microsecond=0) #the run_time should be 2 hours before the now time since it takes less than 2 hours for a new hrrr forecast to be readily available for use
+    
+    
+    # now_utc = datetime.utcnow()
+    # target_utc = now_utc.replace(hour=10, minute=0, second=0, microsecond=0)
+    # target_date_str = (target_utc - timedelta(hours=6)).strftime('%A, %b %d') # CST Date
+    central = pytz.timezone('US/Central')
+    now_utc = datetime.now(pytz.utc)
+    now_cst = datetime.now(pytz.utc).astimezone(central)
+    if current_run_time is None:
+        run_time = (datetime.now(pytz.utc) - timedelta(hours=1)).replace(minute=0, second=0, microsecond=0)
+    else:
+        run_time = current_run_time
+    if run_time.tzinfo is None:
+        run_time = run_time.replace(tzinfo=pytz.UTC)
+    
+    if now_cst.hour >= 3:
+        target_date = now_cst.date() + timedelta(days=1)
+    else:
+        target_date = now_cst.date()
+    
+    target_utc = pytz.utc.localize(datetime.combine(target_date, time(10, 0)))
+    target_local_time = target_utc.astimezone(central)
+    display_date_str = (target_local_time - timedelta(days = 1)).strftime('%A, %b %d')
+    
     fxx = int((target_utc - run_time).total_seconds() // 3600)
     if fxx > 18:
         # Find the most recent multiple of 6 UTC run
@@ -47,7 +65,7 @@ def run_forecast(BOG_LAT, BOG_LON,current_run_time = None):
         fxx = int((target_utc - run_time).total_seconds() // 3600)
         print(f"Long lead time ({fxx}h) detected. Snapping to extended run: {run_time}")
     try:
-        H = Herbie(run_time, model='hrrr', product='sfc', fxx=fxx)
+        H = Herbie(run_time.replace(tzinfo=None), model='hrrr', product='sfc', fxx=fxx)
         # Attempt to load the index to verify it's on the server
         _ = H.index_as_dataframe
     except Exception:
@@ -56,7 +74,7 @@ def run_forecast(BOG_LAT, BOG_LON,current_run_time = None):
         rollback = 6 if fxx > 18 else 1
         run_time = run_time - timedelta(hours=rollback)
         fxx = int((target_utc - run_time).total_seconds() // 3600)
-        H = Herbie(run_time, model='hrrr', product='sfc', fxx=fxx)
+        H = Herbie(run_time.replace(tzinfo=None), model='hrrr', product='sfc', fxx=fxx)
     print (f"\nForecast time: {target_utc} UTC & {target_utc - timedelta(hours=5)} EST\nRun Time found: {run_time} UTC & {run_time - timedelta(hours=5)} EST\nApprox. lead time to forecast minimum temperature: {fxx} hours\n")
     try: #this try-except block is to ensure there is no corrupted file in the local system with index so when the system checks the index is there but the file is corrupted/missing. In that case, the except block redownloads the hrrr data
         # overwrite=True is used to fix the FileNotFoundError
@@ -99,14 +117,24 @@ def run_forecast(BOG_LAT, BOG_LON,current_run_time = None):
         "hrrr_tcc": round(inputs['ForeCC_4AM_Site_HRRR'].iloc[0], 1),
         "run_time": run_time.strftime('%H:%M UTC'),
         "fxx": fxx,
-        "target_date": target_date_str
+        "target_date": display_date_str
         }
 
 def get_hrrr_curve(BOG_LAT, BOG_LON, run_time):
     points_df = pd.DataFrame({"latitude": [BOG_LAT], "longitude": [BOG_LON]}) 
+    central = pytz.timezone('US/Central')
+    now_utc = datetime.now(pytz.utc)
+    now_cst = now_utc.astimezone(central)
+    
+    if now_cst.hour >= 3:
+        target_morning_date = now_cst.date() + timedelta(days=1)
+    else:
+        target_morning_date = now_cst.date()
+        
+        
     # 9 PM CST is 03:00 UTC (Next Day); 6 AM CST is 12:00 UTC (Next Day)
     # Logic to find the UTC timestamp for 9 PM Today
-    target_start_utc = (run_time.replace(hour=0, minute=0) + timedelta(days=1)).replace(hour=3)
+    target_start_utc = pytz.utc.localize(datetime.combine(target_morning_date, time(3, 0)))
     # Calculate starting fxx
     start_fxx = int((target_start_utc - run_time).total_seconds() // 3600)
     curve_data = []
@@ -115,18 +143,23 @@ def get_hrrr_curve(BOG_LAT, BOG_LON, run_time):
         f = start_fxx + i
         # HRRR standard runs go max to 18 hours (longer lead times are extended runs)
         # If we need longer, we have the 00/06/12/18z runs.
-        if f > 18:
-            break 
+        if f > 18 or f < 0:
+            continue 
         try:
-            H = Herbie(run_time, model='hrrr', product='sfc', fxx=f)
+            point_utc = run_time + timedelta(hours=f)
+            point_cst = point_utc.astimezone(central)
+            if point_cst <= now_cst:
+                continue
+            H = Herbie(run_time.replace(tzinfo=None), model='hrrr', product='sfc', fxx=f)
             ds = H.xarray(r":TMP:surface")
             p = ds.herbie.pick_points(points_df, method="weighted")
             temp_f = (float(p.t.values[0]) - 273.15) * 1.8 + 32
             
             # Convert valid time to CST for display
             valid_time_cst = (run_time + timedelta(hours=f)) - timedelta(hours=6)
+            
             curve_data.append({
-                "Time": valid_time_cst.strftime("%I %p"), 
+                "Time": point_cst.strftime("%I %p"), 
                 "Temp": round(temp_f, 1)
             })
         except:
@@ -139,7 +172,7 @@ def get_hrrr_curve(BOG_LAT, BOG_LON, run_time):
 
 # %% streamlit APP CODE SECTION
 
-st.set_page_config(page_title="Marsh Frost Forecast Tool", layout="wide")
+st.set_page_config(page_title="Cranberry Frostcast", layout="wide")
 # st.set_page_config(page_title="Bog Frost Forecast Tool", layout="wide")
 
 # CACHING THE MODEL - We load the model once and keep it in memory
@@ -159,24 +192,26 @@ def get_prediction(lat, lon,current_run_time):
 def get_cached_hrrr_curve(lat, lon, current_run_time):
     return get_hrrr_curve(lat, lon, current_run_time)
 
+#reduce white spacing in the page
+st.markdown("""
+    <style>
+        .block-container {
+            padding-top: 2rem;
+            padding-bottom: 0rem;
+        }
+    </style>
+""", unsafe_allow_html=True)
 
-#  UI LAYOUT 
-st.title("Marsh Frost Forecasting Tool ❄️")
-# st.title("Bog Frost Forecasting Tool ❄️")
-st.info("Current Time: " + datetime.utcnow().strftime('%H:%M') + " UTC (" + (datetime.utcnow()- timedelta(hours=6)).strftime('%H:%M') + " CST)")
+st.title("Cranberry Marsh Frostcast ❄️")
+
 # st.info("Current Time: " + datetime.utcnow().strftime('%H:%M') + " UTC (" + (datetime.utcnow()- timedelta(hours=5)).strftime('%H:%M') + " EST)")
-# if st.sidebar.button("Force Refresh Data"):
-#     st.cache_data.clear()
-#     st.rerun() 
-now_utc = datetime.utcnow()
-# We assume the HRRR run from 2 hours ago is the most recent stable one available
-latest_run_time = (now_utc - timedelta(hours=2)).replace(minute=0, second=0, microsecond=0)
+
 col1, col2 = st.columns([1, 2])
 with col1:
     # st.header("1. Marsh Location")
-    st.header("1. Bog Location")
-    # site_name = st.text_input("Marsh Name", "Copper River Marsh")
-    site_name = st.text_input("Bog Name", "Copper River Marsh")
+    st.header("1. Enter Farm Location")
+    site_name = st.text_input("Marsh Name", "Copper River Marsh")
+    # site_name = st.text_input("Bog Name", "Copper River Marsh")
     lat = st.number_input("Latitude", value=45.207653, format="%.6f")
     lon = st.number_input("Longitude", value=-89.86566, format="%.6f")
     tol = st.number_input("Tolerance", value=32.0, format="%.1f",step = 0.5)
@@ -193,6 +228,11 @@ st.write("""
     Frost protection decisions should be made based on multiple data sources, 
     including on-site thermometers.
 """)   
+now_utc = datetime.now(pytz.utc)
+# We assume the HRRR run from 2 hours ago is the most recent stable one available
+latest_run_time = (now_utc - timedelta(hours=1)).replace(minute=0, second=0, microsecond=0)
+
+# .replace(tzinfo=None) with all herbie lines H = Herbie(latest_run_time.replace(tzinfo=None), model='hrrr', product='sfc', fxx=fxx)
 
 if predict_btn:
     st.session_state.show_results = True
@@ -204,23 +244,23 @@ if st.session_state.get('show_results'):
     with result_container:
         metrics_placeholder = st.empty()
         chart_placeholder = st.empty()
-
+        
         # The map embedded in the window before forecast and hrrr curve in its own window
     with st.expander("Hourly Cloud Movement (Forecast by NOAA NDFD)", expanded=True):
         # creating a Time Slider for the next 12 hours using local time but UTC used for request data from NOAA
         central_tz = pytz.timezone('US/Central')
-        central_now = datetime.now(pytz.utc).astimezone(central_tz) #In central time for displaying only
+        central_now_CCmap = datetime.now(pytz.utc).astimezone(central_tz) #In central time for displaying only
         # The user can choose between 0 - 12 hours to look at cloud cover forecast
         hour_offset = st.slider("Forecast Hour Offset", 0, 12, 0, help="Slide to see how clouds move over the next 12 hours.")
          
         # Calculate target times
-        target_local = central_now + timedelta(hours=hour_offset)
-        target_utc = datetime.utcnow() + timedelta(hours=hour_offset)
+        target_local_CCmap = central_now_CCmap + timedelta(hours=hour_offset)
+        target_utc_CCmap = datetime.utcnow() + timedelta(hours=hour_offset)
         
         # Format for the Map Request (UTC) and the Label (Local)
-        vtit_time = target_utc.strftime("%Y-%m-%dT%H:00")
+        vtit_time = target_utc_CCmap.strftime("%Y-%m-%dT%H:00")
         # display_label = target_local.strftime("%I:00 %p %b %d")
-        display_label = (target_local).strftime("%B %d, %I:00 %p") # This display is according to central or CST/CDT. So there will be one hour lag from NOAA interface if accessed in Mass.
+        display_label = (target_local_CCmap).strftime("%B %d, %I:00 %p") # This display is according to central or CST/CDT. So there will be one hour lag from NOAA interface if accessed in Mass.
         st.subheader(f"Cloud cover Forecast for {display_label}")
     
         # Setup Map
@@ -249,13 +289,20 @@ if st.session_state.get('show_results'):
         ).add_to(m)
         
         # Legend
-        st.markdown("""
-    **Sky Cover Interpretation:**
-    * **Deep Blue: (0-25%)**: Clear Sky → **Maximum Frost Potential**
-    * **White/Pale Blue (25-50%)**: Light Haze → **Moderate Frost Risk**
-    * **Light Grey (50-75%)**: Partial Blanket → **Reduced Risk**
-    * **Dark Grey (75-100%)**: Full Overcast → **Thermal Blanket**
-    """)
+        # Create 4 equal columns
+        leg_col1, leg_col2, leg_col3, leg_col4 = st.columns(4)
+        
+        with leg_col1:
+            st.markdown("**Deep Blue:** \nClear Sky (0-25%)")
+        
+        with leg_col2:
+            st.markdown("**White/Pale Blue:** \nLight (25-50%)")
+        
+        with leg_col3:
+            st.markdown("**Light Gray:** \nModerate (50-75%)")
+        
+        with leg_col4:
+            st.markdown("**Dark Gray:** \nOvercast (75-100%)")
         # Rendering the map
         st_folium(m, height=600, use_container_width=True, key=f"map_{hour_offset}")
         
@@ -275,17 +322,17 @@ if st.session_state.get('show_results'):
         
         m1, m2, m3 = st.columns(3)
         m1.metric("Predicted Minimum Marsh Temperature", f"{res['prediction']}°F")
-        st.caption(f"Based on Tolerance: {tol:.1f}°F")
+        # st.caption(f"Based on Tolerance: {tol:.1f}°F")
         m2.metric("HRRR Surface Temp", f"{res['hrrr_temp']}°F")
         m3.metric("Total Cloud Cover", f"{res['hrrr_tcc']}%")
         if res['prediction'] <= tol - 5:
-            st.error(f"⚠️ **HIGH FROST RISK**: Predicted temperature is {res['prediction']:.1f}°F, which is {diff:.1f}°F {aorb} Tolerance.")
+            st.error(f"⚠️ **HIGH FROST RISK**: Predicted temperature is {res['prediction']:.1f}°F, which is {diff:.1f}°F {aorb} Tolerance ({tol:.1f}°F).")
         elif tol - 5 < res['prediction'] <= tol:
-            st.error(f"⚠️ **FROST RISK**: Predicted temperature is {res['prediction']:.1f}°F, which is {diff:.1f}°F {aorb} Tolerance.")
+            st.error(f"⚠️ **FROST RISK**: Predicted temperature is {res['prediction']:.1f}°F, which is {diff:.1f}°F {aorb} Tolerance ({tol:.1f}°F).")
         elif tol < res['prediction'] <= tol + 5:
-            st.info(f"⚠️ **FROST POSSIBLE**: Predicted temperature is {res['prediction']:.1f}°F, which is {diff:.1f}°F {aorb} Tolerance.")
+            st.info(f"⚠️ **FROST POSSIBLE**: Predicted temperature is {res['prediction']:.1f}°F, which is {diff:.1f}°F {aorb} Tolerance ({tol:.1f}°F).")
         elif res['prediction'] > tol + 5:
-            st.success(f"✅ **LOW RISK**: Conditions currently look safe. Predicted temperature is {res['prediction']:.1f}°F, which is {diff:.1f}°F {aorb} Tolerance.")
+            st.success(f"✅ **LOW RISK**: Conditions currently look safe. Predicted temperature is {res['prediction']:.1f}°F, which is {diff:.1f}°F {aorb} Tolerance ({tol:.1f}°F).")
         # Display the Hourly HRRR Curve
         st.markdown("### Overnight Temperature Trend using NOAA's HRRR regional model")
         df_curve = get_cached_hrrr_curve(lat, lon, latest_run_time)
@@ -293,7 +340,7 @@ if st.session_state.get('show_results'):
             plot_col, _ = st.columns([0.8, 0.2])
             with plot_col:
                 fig = px.line(df_curve, x="Time", y="Temp", 
-                              labels={"Temp": "Temperature (°F)", "Time": "Time (CST)"},
+                              labels={"Temp": "Temperature (°F)", "Time": "Time"},
                               markers=True)
                 # Update the Curve and Markers
                 fig.update_traces(line_color='#2E86C1', line_width=4, marker=dict(size=10))
